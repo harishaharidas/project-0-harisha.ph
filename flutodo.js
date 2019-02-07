50bfa058e328e1d7975d5f76b194d269164f17fb
unknown <harisha.ph@PIA3064.pitsolutions.com>
2019-02-07 13:47:50 +0530

    include sort

--- flutodo.js
+++ flutodo.js
@@ -1,84 +1,72 @@
 const myNodelist = document.getElementsByClassName("sub");
 for (let i = 0; i < myNodelist.length; i++) {
-    let span = document.createElement("SPAN");
-    let txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    myNodelist[i].appendChild(span);
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
 }
 
 const close = document.getElementsByClassName("close");
 for (let i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-        let div = this.parentElement;
-        div.style.display = "none";
-    }
+  close[i].onclick = function () {
+    let div = this.parentElement;
+    div.style.display = "none";
+  }
 }
 
 const list = document.querySelector('#div2');
 list.addEventListener('click', function (ev) {
-    if (ev.target.className === 'sub' || 'sub checked') {
-        ev.target.classList.toggle('checked');
-    }
+  if (ev.target.className === 'sub' || 'sub checked') {
+    ev.target.classList.toggle('checked');
+  }
 }, false);
 
 function newElement() {
-    let li1 = document.createElement("div");
-    li1.className = "sub";
-    let inputValue = document.getElementById("title").value;
-    let t = document.createTextNode(inputValue);
-    li1.appendChild(t);
-    if (inputValue === '') {
-        alert("You must write something!");
-    } else {
-        let list1 = document.getElementById("div2");
-        list1.insertBefore(li1, list1.childNodes[0]);
-    }
-    document.getElementById("title").value = "";
+  let li1 = document.createElement("div");
+  li1.className = "sub";
+  let inputValue = document.getElementById("title").value;
+  let t = document.createTextNode(inputValue);
+  li1.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    let list1 = document.getElementById("div2");
+    list1.insertBefore(li1, list1.childNodes[0]);
+  }
+  document.getElementById("title").value = "";
 
-    let span = document.createElement("SPAN");
-    let txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li1.appendChild(span);
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li1.appendChild(span);
 
-    for (let i = 0; i < close.length; i++) {
-        close[i].onclick = function () {
-            var div = this.parentElement;
-            div.style.display = "none";
-        }
+  for (let i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      var div = this.parentElement;
+      div.style.display = "none";
     }
+  }
 }
 
 function sortList() {
-    var list, i, switching, b, shouldSwitch;
-    list = document.getElementById("div2");
-    switching = true;
-    /* Make a loop that will continue until
-    no switching has been done: */
-    while (switching) {
-      // start by saying: no switching is done:
-      switching = false;
-      b = list.getElementsByClassName("sub");
-      // Loop through all list-items:
-      for (i = 0; i < (b.length - 1); i++) {
-        // start by saying there should be no switching:
-        shouldSwitch = false;
-        /* check if the next item should
-        switch place with the current item: */
-        if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
-          /* if next item is alphabetically
-          lower than current item, mark as a switch
-          and break the loop: */
-          shouldSwitch = true;
-          break;
-        }
-      }
-      if (shouldSwitch) {
-        /* If a switch has been marked, make the switch
-        and mark the switch as done: */
-        b[i].parentNode.insertBefore(b[i + 1], b[i]);
-        switching = true;
+  var list, i, switching, b, shouldSwitch;
+  list = document.getElementById("div2");
+  switching = true;
+  while (switching) {
+    switching = false;
+    b = list.getElementsByClassName("sub");
+    for (i = 0; i < (b.length - 1); i++) {
+      shouldSwitch = false;
+      if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+        shouldSwitch = true;
+        break;
       }
     }
+    if (shouldSwitch) {
+      b[i].parentNode.insertBefore(b[i + 1], b[i]);
+      switching = true;
+    }
   }
+}
